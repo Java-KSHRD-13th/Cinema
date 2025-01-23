9dcf443a824741ced14141a1633392b7e8ea4f80
Kouern Doch <dochkouern@gmail.com>
2025-01-23 21:30:19 +0700

    lastUpdate

--- src/Main.java
+++ src/Main.java
@@ -32,6 +32,8 @@ public class Main {
         int countMovieBooked = 0;
         int movieID = 0;
         boolean checkBooking = false;
+        int seat;
+        String movieIDString;
 
 
         // color
@@ -47,8 +49,8 @@ public class Main {
         do {
             System.out.print("\tEnter number of Hall in Cinema: ");
             numberString = scanner.nextLine();
-            if(!numberString.matches("[0-9]+")){
-                System.out.println(RED+"\tNumber of hall is allow only number!"+RESET);
+            if(!numberString.matches("[0-9]+")|| Integer.parseInt(numberString)<=0){
+                System.out.println(RED+"\tNumber of hall is allow only number and more then 0!"+RESET);
                 checkAgain = true;
             }else{
                 checkAgain = false;
@@ -58,8 +60,8 @@ public class Main {
         do{
         System.out.print("\tEnter number of seat in each Hall: ");
         numberString = scanner.nextLine();
-        if(!numberString.matches("[0-9]+")){
-            System.out.println(RED+"\tNumber of seat is allow only number!"+RESET);
+        if(!numberString.matches("[0-9]+") || Integer.parseInt(numberString)<=0){
+            System.out.println(RED+"\tNumber of seat is allow only number and more then 0!"+RESET);
             checkAgain = true;
         }else {
             checkAgain = false;
@@ -69,8 +71,9 @@ public class Main {
      //   Menu main Feature
 
         newHallArray = new String[numberHall][numberSeat];
-        availableSeat=new int[numberHall];
-        unAvailableSeat=new int[numberHall];
+        availableSeat = new int[numberHall];
+        unAvailableSeat = new int[numberHall];
+
         for (int i = 0; i < numberHall; i++) {
             for (int j = 0; j < numberSeat; j++) {
                 newHallArray[i][j] = "+";
@@ -203,8 +206,17 @@ public class Main {
                             switch (optionString) {
                                 case "1":
                                     // New code
-                                    System.out.print("Enter Movie ID: ");
-                                    movieID = Integer.parseInt(scanner.nextLine());
+                                    while (true) {
+                                        System.out.print("Enter Movie ID: ");
+                                        movieIDString = scanner.nextLine();
+                                        if(Pattern.matches("[0-9]+", movieIDString) && Integer.parseInt(movieIDString)<=cn){
+                                            movieID = Integer.parseInt(movieIDString);
+                                            break;
+                                        }else {
+                                            System.out.println(RED+"Please enter a valid movie ID! "+RESET);
+                                        }
+                                    }
+
                                     Table t1 = new Table(15, BorderStyle.UNICODE_BOX_DOUBLE_BORDER, ShownBorders.ALL);
                                     for (int i = 0; i < 15; i++) {
                                         t1.setColumnWidth(i, 10, 20);
@@ -226,23 +238,26 @@ public class Main {
                                                 System.out.print("Choose seat that you want to booking(e.g:1,2,3,4): ");
                                                 String bookingID = scanner.nextLine();
                                                 String[] parts = bookingID.split(",");
+
                                                 for (int i = 0; i < parts.length; i++) {
-                                                    if(Pattern.matches("[0-9]+", parts[i])) {
-                                                        int seat = Integer.parseInt(parts[i]);
-                                                        newHallArray[movieID - 1][seat - 1] = "-";
-                                                        unAvailableSeat[movieID - 1]++;
-                                                        availableSeat[movieID - 1]--;
+                                                    if(Pattern.matches("[0-9]+", parts[i]) && (Integer.parseInt(parts[i]) < newHallArray[movieID - 1].length && Integer.parseInt(parts[i]) > 0)){
                                                         checkBooking = true;
                                                     }else{
                                                         checkBooking = false;
                                                         System.out.println(RED+"Not allow input String and over then number of seat!"+RESET);
+                                                        break;
                                                     }
-
                                                 }
                                                 if(checkBooking){
+                                                    for (int i = 0; i < parts.length; i++) {
+                                                        seat = Integer.parseInt(parts[i]);
+                                                        newHallArray[movieID - 1][seat - 1] = "-";
+                                                        unAvailableSeat[movieID - 1]++;
+                                                        availableSeat[movieID - 1]--;
+                                                        checkBooking = true;
+                                                    }
                                                     break ;
                                                 }
-
                                             }
 
                                             checkAgain = true;
@@ -318,18 +333,31 @@ public class Main {
 
                     }
 
-
                     break;
                 case 4:
-                    newHallArray = new String[numberHall][numberSeat];
+                    unavailable = 0;
                     for (int i = 0; i < newHallArray.length; i++) {
                         for (int j = 0; j < numberSeat; j++) {
                             newHallArray[i][j] = "+";
+                            available++;
                         }
-                        System.out.println("All Hall is already reset!");
+                        availableSeat[i]=available;
+                        unAvailableSeat[i] = unavailable;
+                        available = 0;
                     }
+                    System.out.println("All Hall is already reset!");
                     break;
                 case 5:
+                          while (true) {
+                              System.out.print("=> Enter number of row to show record : ");
+                              String number = scanner.nextLine();
+                                if (Pattern.matches("[1-9]+", number)) {
+                                    pageSize = Integer.parseInt(scanner.nextLine());
+                                    break;
+                                }else{
+                                    System.out.println(RED+"Not allow input String and less then 1!"+RESET);
+                                }
+                          }
                     break;
                 case 6:
                     System.out.println("Good bye :)");
